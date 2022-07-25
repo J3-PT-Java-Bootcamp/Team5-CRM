d4a3893ac6b6b368c39bd6400de475541bebd163
Alex Arraiza <alexarraiza@gmail.com>
2022-07-25 22:25:02 +0200

    Add new repository system Set up this new repository with the existing menu

---
+++ src/test/java/com/ironhack/business_logic/AccountServiceTest.java
@@ -0,0 +1,12 @@
+package com.ironhack.business_logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountServiceTest {
+
+    @Test
+    void saveAccount() {
+    }
+}
