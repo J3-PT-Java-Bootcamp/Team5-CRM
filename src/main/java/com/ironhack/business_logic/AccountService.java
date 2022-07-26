c181363d6cf2802a1ac80758b4e3025760287e43
isaDevs <isabel.calzadilla.18@gmail.com>
2022-07-26 17:36:45 +0200

    ADD THE CLOSE-LOST , CLOS-WON, OPEN METHODS TO OPPORTUNITY SERVICE

--- src/main/java/com/ironhack/business_logic/AccountService.java
+++ src/main/java/com/ironhack/business_logic/AccountService.java
@@ -1,7 +1,16 @@
 package com.ironhack.business_logic;
 
 import com.ironhack.data.AccountRepository;
+import com.ironhack.data.datasources.impl.InMemoryDatasource;
+import com.ironhack.data.exceptions.DataNotFoundException;
 import com.ironhack.domain.Account;
+import com.ironhack.domain.Contact;
+import com.ironhack.domain.Opportunity;
+import com.ironhack.domain.enums.Product;
+import com.ironhack.domain.enums.Status;
+
+import javax.swing.*;
+import java.util.ArrayList;
 
 public class AccountService {
 
@@ -9,6 +18,8 @@ public class AccountService {
 
     private final AccountRepository accountRepository;
 
+    private ArrayList <Contact> contacts = new ArrayList<>();
+    private ArrayList <Opportunity> opportunities = new ArrayList<>();
     private AccountService(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
     }
@@ -21,8 +32,22 @@ public class AccountService {
     }
 
     public Account saveAccount(Account account) {
+        //var id = accountRepository
+
         accountRepository.saveAccount(account);
         return account;
+    }
+
+
+    // new methods added
+    public ArrayList contactList(Contact contact){
+        contacts.add(contact);
+        return contacts;
+    }
+
+    public ArrayList opportunityList(Opportunity opportunity){
+        opportunities.add(opportunity);
+        return opportunities;
     }
 
 }
