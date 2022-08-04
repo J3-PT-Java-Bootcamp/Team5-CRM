200eb1cbddc5a7f63461be6f6e3d252a2ac283d4
Alex Arraiza <alexarraiza@gmail.com>
2022-08-04 19:27:09 +0200

    setup services test scaffold

---
+++ src/test/java/com/ironhack/services/LeadServiceTest.java
@@ -0,0 +1,33 @@
+package com.ironhack.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeadServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void newLead() {
+    }
+
+    @Test
+    void convert() {
+    }
+
+    @Test
+    void getAllLeads() {
+    }
+
+    @Test
+    void lookUpLead() {
+    }
+}
---
+++ src/test/java/com/ironhack/services/OpportunityServiceTest.java
@@ -0,0 +1,29 @@
+package com.ironhack.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OpportunityServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void getAllOpportunities() {
+    }
+
+    @Test
+    void lookUpOpportunity() {
+    }
+
+    @Test
+    void updateOpportunityStatus() {
+    }
+}
