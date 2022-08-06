020a00539c849ec5cc4e4007376dca2eb98be668
Alex Arraiza <alexarraiza@gmail.com>
2022-08-06 11:30:51 +0200

    Fix status change

--- src/main/java/com/ironhack/ui/ConsoleOperations.java
+++ src/main/java/com/ironhack/ui/ConsoleOperations.java
@@ -1,6 +1,5 @@
 package com.ironhack.ui;
 
-
 public interface ConsoleOperations {
 
     // Operations to create or find leads / opportunities
@@ -9,10 +8,10 @@ public interface ConsoleOperations {
     String SHOW = "show";
     String LOOKUP = "lookup";
 
-    //Operations for Statuses
-    String OPEN = "Open";
-    String CLOSE_LOST = "Close-Lost";
-    String CLOSE_WON = "Close-Won";
+    // Operations for Statuses
+    String OPEN = "open";
+    String CLOSE_LOST = "close-lost";
+    String CLOSE_WON = "close-won";
 
     // Operations for Products
     String HYBRID = "Hybrid";
@@ -25,6 +24,5 @@ public interface ConsoleOperations {
     String MANUFACTURING = "Manufacturing";
     String MEDICAL = "Medical";
     String OTHER = "Other Industries";
-
 
 }
